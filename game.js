eb669a32787044c06db5d5d9d4a16aa887bb99e5
Mari4761 <marinam4761@gmail.com>
2017-07-27 12:06:58 -0700

    initial commit

---
+++ game.js
@@ -0,0 +1,35 @@
+var bg;
+var y = 0;
+var gb;
+
+function setup() {
+ 
+  bg = loadImage("side-chick.jpg", function (img){
+      image(img,60,0);
+  });
+ // var canvas= createCanvas(720, 400);
+ // background("#CCCCCC")
+  gb = loadImage("plastic-water-bottle.jpg", function (bottle){
+      image(bottle,0,0);
+  });
+var canvas= createCanvas(720,400);
+  background("#CCCCCC")
+  
+}
+
+function draw() {
+
+}
+ function makeBottle() {
+ loadImage("plastic-water-bottle.jpg", function (img){
+//       image(img,0,0);
+//   });
+//   var canvas= createCanvas(720,400);
+//   background("#CCCCCC");
+//   
+    
+//}
+
+function makeBottle() {
+    
+}
