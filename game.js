7de8333dc90d6de131feed8e46392aca8069b0ac
Schyel <schyelbrackins1@gmail.com>
2017-08-01 09:57:59 -0700

    add hearts

--- game.js
+++ game.js
@@ -2,15 +2,18 @@ var bg, sc;
 var y = 0;
 var gb;
 var person;
+var heart ;
+var eheart;
+var offset;
 
 function preload() {
   bg = loadImage("city-background.jpg");
   // Monospace =  loadFont(monospace);
   plasticBottle = loadImage("plastic-water-bottle.jpg");
   sc = loadImage("girl.png");
-
+  eheart= loadImage("EmptyHeart23.png")
+  heart = loadImage("heart23.png")
 }
-
 
 function keyPressed() {
     if (key === " ") {
@@ -19,13 +22,20 @@ function keyPressed() {
     }
 }
 
-// function draw() {
-//   background(51);
-//
-//   translate(-person.pos.x+50, 0);
-//
-//
-// }
+image(bg)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function setup() {
   offset = 0;
@@ -40,6 +50,7 @@ function draw() {
         offset=0;
     }
     dis_money();
+    showLives();
     translate(-person.pos.x+50, 0);
     var gravity = createVector(0, 0.1);
     person.applyForce(gravity);
@@ -47,15 +58,45 @@ function draw() {
     person.update();
     person.edges();
     person.display(sc);
-    // loadImage("side-chick.jpg", function(sc){
-    //   person.display(sc);
-    //   image(sc)
-    // })
-
 
 
     fill(255, 0, 100);
     rect(400, height-50, 50, 50);
+}
+
+function showLives() {
+  image(heart, 0,0,30,30)
+  image(heart,29,0,30,30)
+  image(heart,58,0,30,30)
+  image(heart,88,0,30,30)
+  image(heart,118,0,30,30)
+
+  if (lives < 1 ) {
+    image(eheart, 0,0,30,30)
+  } else {
+    image(heart, 0,0,30,30)
+  }
+  if (lives < 2 ) {
+    image(eheart, 29,0,30,30)
+  } else {
+    image(heart, 29,0,30,30)
+  }
+  if (lives < 3 ) {
+    image(eheart, 58,0,30,30)
+  } else {
+    image(heart, 58,0,30,30)
+  }
+  if (lives < 4 ) {
+    image(eheart, 88,0,30,30)
+  } else {
+    image(heart, 88,0,30,30)
+  }
+  if (lives < 5 ) {
+    image(eheart, 118,0,30,30)
+  } else {
+    image(heart, 118,0,30,30)
+  }
+
 }
 
 function clear(){
