2c105d17d41ee3d3525ff8a7e755253fc957715e
Silvia <silviac.4474@gmail.com>
2017-08-01 10:21:41 -0700

    make recycling center to move

--- game.js
+++ game.js
@@ -31,7 +31,7 @@ function keyPressed() {
 function setup() {
   offset =0;
   createCanvas(839, 350);
-  person = new Person();
+   person = new Person();
 
 }
 
@@ -46,20 +46,24 @@ function draw() {
     if (offset<=-width){
         offset=0;
     }
-    if (millis()>100000){
-      image(rc, 400, height/2, rc.width/4, rc.height/4);
-    }
+
+
     dis_money();
     translate(-person.pos.x+50, 0);
+
+    if (millis()>60000){
+      image(rc, 6000, height/2, rc.width/4, rc.height/4);
+    }
+
     var gravity = createVector(0, 0.1);
     person.applyForce(gravity);
     if (person.pos.y == 350) {
       jumps =0;
     }
 
-    person.update();
-    person.edges();
-    person.display(sc);
+     person.update();
+     person.edges();
+     person.display(sc);
 
     fill(255, 0, 100);
     rect(400, height-50, 50, 50);
